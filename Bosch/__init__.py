ad3ea89304e566382b48138fb67a8766e44e797a
Samarth Singh <singhsamarth2000@gmail.com>
2021-03-22 19:55:43 +0530

    added preprocessing

--- Bosch/__init__.py
+++ Bosch/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, url_for
 from flask_restful import Api, Resource
+from Bosch.preprocess import load_and_preprocess
 
 app = Flask(__name__)
 
@@ -8,6 +9,7 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 
 @app.route('/')
 def hdfd():
+    input_data, input_labels = load_and_preprocess()
     return f"Running... "
 
 from Bosch.Resources import basicAPIS
---
+++ Bosch/model.py
@@ -0,0 +1,80 @@
+import time
+import numpy as np 
+import pandas as pd
+import os
+import cv2
+from PIL import Image
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+np.random.seed(42)
+tf.random.set_seed(42)
+
+classes = { 1:'Speed limit (20km/h)',
+            2:'Speed limit (30km/h)', 
+            3:'Speed limit (50km/h)', 
+            4:'Speed limit (60km/h)', 
+            5:'Speed limit (70km/h)', 
+            6:'Speed limit (80km/h)', 
+            7:'End of speed limit (80km/h)', 
+            8:'Speed limit (100km/h)', 
+            9:'Speed limit (120km/h)', 
+            10:'No passing', 
+            11:'No passing veh over 3.5 tons', 
+            12:'Right-of-way at intersection', 
+            13:'Priority road', 
+            14:'Yield', 
+            15:'Stop', 
+            16:'No vehicles', 
+            17:'Veh > 3.5 tons prohibited', 
+            18:'No entry', 
+            19:'General caution', 
+            20:'Dangerous curve left', 
+            21:'Dangerous curve right', 
+            22:'Double curve', 
+            23:'Bumpy road', 
+            24:'Slippery road', 
+            25:'Road narrows on the right', 
+            26:'Road work', 
+            27:'Traffic signals', 
+            28:'Pedestrians', 
+            29:'Children crossing', 
+            30:'Bicycles crossing', 
+            31:'Beware of ice/snow',
+            32:'Wild animals crossing', 
+            33:'End speed + passing limits', 
+            34:'Turn right ahead', 
+            35:'Turn left ahead', 
+            36:'Ahead only', 
+            37:'Go straight or right', 
+            38:'Go straight or left', 
+            39:'Keep right', 
+            40:'Keep left', 
+            41:'Roundabout mandatory', 
+            42:'End of no passing', 
+            43:'End no passing veh > 3.5 tons',
+            44:'Dangerous Dip',
+            45:'No Parking',
+            46:'Horn Prohibited',
+            47:'School Ahead',
+            48:'Falling Rocks' }
+
+
+def testModel():
+    model = keras.models.Sequential([    keras.layers.Conv2D(filters=16, kernel_size=(5,5), activation='relu', input_shape=(height,width,channels)),
+    keras.layers.Conv2D(filters=32, kernel_size=(3,3), activation='relu'),
+    keras.layers.MaxPool2D(pool_size=(2, 2)),
+    keras.layers.Dropout(rate=0.25),
+    keras.layers.Conv2D(filters=64, kernel_size=(3,3), activation='relu'),
+    keras.layers.Conv2D(filters=128, kernel_size=(3,3), activation='relu'),
+    keras.layers.MaxPool2D(pool_size=(2, 2)),
+    keras.layers.Dropout(rate=0.25),
+    keras.layers.Flatten(),
+    keras.layers.Dense(512, activation='relu'),
+    keras.layers.Dropout(rate=0.5),
+    keras.layers.Dense(48, activation='softmax')
+    ])
+    return model
+
