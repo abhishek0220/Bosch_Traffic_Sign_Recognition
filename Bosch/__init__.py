365f05e5855a724ee5e97081cc1f1e35f95d9c7d
Samarth Singh <singhsamarth2000@gmail.com>
2021-03-22 22:50:12 +0530

    tested model imports and updated requirements

--- Bosch/__init__.py
+++ Bosch/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, url_for
 from flask_restful import Api, Resource
 #from Bosch.preprocess import load_and_preprocess
+from Bosch.model import testModel
 
 app = Flask(__name__)
 
@@ -9,7 +10,9 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 
 @app.route('/')
 def hdfd():
-    input_data, input_labels = load_and_preprocess()
+    #input_data, input_labels = load_and_preprocess()
+    test_model = testModel()
+    print(test_model.summary())
     return f"Running... "
 
 from Bosch.Resources import basicAPIS
