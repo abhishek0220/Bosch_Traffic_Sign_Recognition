098a59cffd9dbbb710ad246a706611c0ed26a20a
Samarth Singh <singhsamarth2000@gmail.com>
2021-03-24 00:12:28 +0530

    added tasks

--- Bosch/Resources/basicAPIS.py
+++ Bosch/Resources/basicAPIS.py
@@ -1,11 +1,17 @@
 from flask_restful import Resource, reqparse, request
 
+# Send image API, Image in data dict
+# Add categories option
 class sendImage(Resource):
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('imageb64', help = 'This field cannot be blank', required = True)
         parser.add_argument('rotation', help = 'This field cannot be blank', required = True)
         data = parser.parse_args()
+        image = data['imageb64']
+        rotation = data['rotation']
+        
+        # Push into database with appropriate rotation
         return {
             "message" : "Received Req successfully",
             "image" : f"{data['imageb64'][:100]}",
@@ -13,3 +19,8 @@ class sendImage(Resource):
         }
     def get(self):
         return {"message" : "Please post method to send image"}
+
+
+class display_graph(Resource):
+    
+    pass
---
+++ Visual_graphs.md
@@ -0,0 +1,10 @@
+# Visual Graphs
+## Training Visualization
+* Classes v/s Accuracy ( Mayank )
+* History Graphs ( Done )
+* Misclassified Samples ( Arya)
+* Per-class F1 score Visualization ( Shreyas )
+
+## Dataset Visualization ( Samarth )
+* Per Class Frequency
+* Random Image Visualization for each class
