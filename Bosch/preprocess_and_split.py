a6eb673ba11201df3cf4929e0cbfcaee9fa4445e
Mayank Singhal <techmayank2000@gmail.com>
2021-03-23 14:01:57 +0530

    added train_valid_split

--- Bosch/preprocess_and_split.py
+++ Bosch/preprocess_and_split.py
@@ -1,9 +1,11 @@
 import time
-import numpy as np 
+import numpy as np
 import pandas as pd
 import os
 import cv2
 from PIL import Image
+import tensorflow as tf
+from tensorflow import keras
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
@@ -39,19 +41,31 @@ def load_and_preprocess():
             except:
                 print("Error - Image loading")
 
-    #Converting lists into numpy arrays
+    # Converting lists into numpy arrays
     # print(counter)
     image_data = np.array(image_data)
     image_labels = np.array(image_labels)
 
     print(image_data.shape, image_labels.shape)
     end = time.time()
-    print("It has taken", round(end-start,5), "seconds")
+    print("It has taken", round(end-start, 5), "seconds")
 
     shuffle_indexes = np.arange(image_data.shape[0])
     np.random.shuffle(shuffle_indexes)
     image_data = image_data[shuffle_indexes]
     image_labels = image_labels[shuffle_indexes]
-    
-    
+
     return image_data, image_labels
+
+
+def train_valid_splitting(image_data, image_labels):
+    X_train, X_valid, y_train, y_valid = train_test_split(image_data, image_labels, test_size=0.2, random_state=42, shuffle=True)
+
+
+    X_train = X_train/255
+    X_valid = X_valid/255
+
+    y_train = keras.utils.to_categorical(y_train, total_classes)
+    y_valid = keras.utils.to_categorical(y_valid, total_classes)
+
+    return X_train,X_valid,y_train,y_valid
