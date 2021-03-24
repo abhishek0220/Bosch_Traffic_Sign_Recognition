da0400799397f4dc67e020e42ffd31cec3996656
Abhishek Chaudhary <43341786+abhishek0220@users.noreply.github.com>
2021-03-24 10:55:06 +0000

    image split

--- Bosch/push_image.py
+++ Bosch/push_image.py
@@ -6,7 +6,7 @@ def pushImage(imageb64,class_name,rotation):
     try:
         im = Image.open(BytesIO(base64.b64decode(imageb64)))
         im = im.rotate(rotation)
-        im.save("Bosch/test.png")
+        im.save("Bosch/static/test.png")
     except:
         return False
     return True
