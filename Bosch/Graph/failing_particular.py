a6dd07f8dc01fdf878c8a041cfd92aa9715ff6ab
Samarth Singh <singhsamarth2000@gmail.com>
2021-03-25 17:13:19 +0530

    changed failing particular as per PIL

--- Bosch/Graph/failing_particular.py
+++ Bosch/Graph/failing_particular.py
@@ -20,10 +20,10 @@ class show_SIFT_features():
 
     def SIFT_compare(misclassified_class=5,input_path='Bosch/Graph/misclassified.jpeg'):
 
-        fig=plt.figure("Comparing the image")
+        #fig=plt.figure("Comparing the image")
 
         input_path = 'Bosch/Graph/misclassified.jpeg'
-        save_path = os.path.join(os.environ['Bosch'], 'static', 'test_misclassified'+'.png' )
+        save_path = os.path.join('Bosch', 'static')
         image = cv2.imread(input_path)
         gray1 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -31,11 +31,12 @@ class show_SIFT_features():
         sift = cv2.xfeatures2d.SIFT_create(nfeatures=50)
         keypoints_1, descriptors_1 = sift.detectAndCompute(image,None)
 
-        fig.add_subplot(rows, columns, 1)
+        #fig.add_subplot(rows, columns, 1)
         img_1 = cv2.drawKeypoints(gray1,keypoints_1,image)
-        plt.imshow(img_1)
-        plt.axis('off')
-        plt.title("First")
+        Image.fromarray(img_1).save(save_path+"/"+"ms_clsfd.png")
+        # plt.imshow(img_1)
+        # plt.axis('off')
+        # plt.title("First")
 
         path = 'Bosch/GTSRB/Final_Training/Images/' + (str(misclassified_class).zfill(5))
         images = os.listdir(path)
@@ -58,7 +59,7 @@ class show_SIFT_features():
             except:
                 print("Error loading",path+'/'+com_img)
             
-        plt.suptitle("Structural similarity : %.2f" % (s))
+        # plt.suptitle("Structural similarity : %.2f" % (s))
 
         image_compare = cv2.imread(path + '/' + com_img)
         resize_image = cv2.resize(image_compare, (width,height), interpolation = cv2.INTER_AREA)        
@@ -66,13 +67,14 @@ class show_SIFT_features():
         sift2 = cv2.xfeatures2d.SIFT_create()
         keypoints_2, descriptors_2 = sift2.detectAndCompute(image_compare,None)
 
-        fig.add_subplot(rows, columns, 2)
+        # fig.add_subplot(rows, columns, 2)
         img_2 = cv2.drawKeypoints(gray2,keypoints_2,image_compare)
-        plt.imshow(img_2);
-        plt.axis('off')
-        plt.title("Second")    
+        Image.fromarray(img_2).save(save_path+"/"+"ms_clsfd_for.png")
+        # plt.imshow(img_2);
+        # plt.axis('off')
+        # plt.title("Second")    
 
-        fig.savefig(save_path)
+        # fig.savefig(save_path)
 
         #save the image at this path
-        return save_path
+        return {"Misclassified_Image":save_path+"/"+"ms_clsfd.png", "Misclassified_for":save_path+"/"+"ms_clsfd_for.png"}
